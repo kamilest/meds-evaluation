ffda20da527497dd89270d5c47f8ea34cae84dec
kamilest <stankeviciute.kamile@gmail.com>
2024-08-09 15:09:03 -0400

    Binary classification initial commit.

---
+++ src/meds-evaluation/binary_classification.py
@@ -0,0 +1,16 @@
+"""Metrics for binary classification.
+
+These will be based on sklearn implementation, and will be computed on patient and on sample level.
+
+References:
+    https://scikit-learn.org/stable/api/sklearn.metrics.html
+    https://scikit-learn.org/stable/modules/model_evaluation.html#classification-metrics
+"""
+
+# TODO:
+#   * accuracy
+#   * precision/recall curve, F1 score
+#   * ROC, AUROC
+#   * calibration curve?
+
+# TODO: sample-level vs patient-level metrics
